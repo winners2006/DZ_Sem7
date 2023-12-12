28fe71dc48429610893f14e85586aa1b38c62191
Winners2006-2 <winners2006-2@mail.ru>
2023-12-12 12:32:39 +0300

    ToTeacher

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+
+int numberM = ReadUserNumber("Введите число M: ");
+int numberN = ReadUserNumber("Введите число N: ");
+Console.WriteLine(Ackerman(numberM, numberN));
+
+int ReadUserNumber(string userText)
+{
+    Console.Write(userText);
+    int numberUser = Convert.ToInt32(Console.ReadLine());
+    return numberUser;
+}
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Ackerman(m - 1, 1);
+    else
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+
+int startNumber = ReadUserNumber("Введите начальное число массива: ");
+int endNumber = ReadUserNumber("Введите конечное число массива: ");
+int arraySize = ReadUserNumber("Введите размер массива: ");
+int[] array = NewRandomArray(arraySize, startNumber, endNumber);
+Print(array);
+Console.WriteLine();
+Array.Reverse(array);
+Print(array);
+
+int ReadUserNumber(string userText)
+{
+    Console.Write(userText);
+    int numberUser = Convert.ToInt32(Console.ReadLine());
+    return numberUser;
+}
+
+int[] NewRandomArray(int arrSize, int arrStart, int arrEnd){
+    Random rnd = new Random();
+    int[] newArr = new int[arrSize];
+    for (int i = 0; i < arrSize; i++)
+    {
+        newArr[i] = rnd.Next(arrStart, arrEnd);
+    }
+    return newArr;
+}
+
+void Print(int[] array, int current = 0)
+
+{
+    if (current == array.Length) return;
+    else
+    {
+        Console.Write(" " + array[current++]);
+        Print(array, current);
+    }
+}
